7e02a293dd0e75a50f8274d2c756936461dde0d5
= <=>
2024-02-28 06:49:53 +0100

    TURR-35 - TürrAI komponensek hibamentesítése

--- Weboldal/app/turrai/page.jsx
+++ Weboldal/app/turrai/page.jsx
@@ -6,9 +6,31 @@ import { useState } from "react";
 
 const AI = () => {
   const [isHovered, setIsHovered] = useState(true);
+
+  const aiTanarok = [
+    {
+      name: "Foki Zoltán",
+      img: "/images/fokiAI.jpg",
+      href: "/turrai/fokizoltan",
+      caption: "mérnök informatikus",
+    },
+    {
+      name: "Taskovics Péter",
+      img: "/images/taskovicsAI.png",
+      href: "/turrai/taskovicspeter",
+      caption: "mérnök informatikus",
+    },
+    {
+      name: "Varga József",
+      img: "/images/vargaAI.jpg",
+      href: "/turrai/vargajozsef",
+      caption: "mérnök informatikus",
+    },
+  ];
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <div className="my-16">
         <div className="text-center">
           <h1 className="text-6xl">TürrAI</h1>
@@ -17,100 +39,40 @@ const AI = () => {
           </p>
         </div>
         <div className="flex justify-center py-8 gap-5 flex-wrap">
-          <div className="elso">
-            <div className=" border border-black rounded-lg text-center relative  ">
-              <Button
-                onClick={() => (window.location.href = "/turrai/fokizoltan")}
-                className={`absolute ${
-                  isHovered ? "flex" : "hidden"
-                } items-center justify-center w-full h-64 bg-gray-600 opacity-0 hover:opacity-50`}
-              >
-                <p className="font-bold text-md text-blue-200 border hover:bg-gray-700 bg-black/90 opacity-100 border-gray-200 px-5 py-2 rounded-lg">
-                  Csevegés
-                </p>
-              </Button>
-              <div
-                className="relative"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              ></div>
-              <Image
-                width={227}
-                height={200}
-                quality={100}
-                src="/images/fokiAI.jpg"
-                className="min-h-64 max-h-64 min-w-64 max-w-64 rounded-sm hover:opacity-45 hover:bg-gray-500 "
-              />
-              <div className="my-3">
-                <h1>Foki Zoltán</h1>
-                <p className="text-foreground/80">mérnök informatikus</p>
+          {aiTanarok.map((tanar) => {
+            return (
+              <div key={tanar}>
+                <div className=" border border-black rounded-lg text-center relative  ">
+                  <Button
+                    onClick={() => (window.location.href = `${tanar.href}`)}
+                    className={`absolute ${
+                      isHovered ? "flex" : "hidden"
+                    } items-center justify-center w-full -top-2 -left-1 h-4/5 bg-gray-600 opacity-0 hover:opacity-50`}
+                  >
+                    <p className="font-bold text-md text-blue-200 border hover:bg-gray-700 bg-black/90 opacity-100 border-gray-200 px-5 py-2 rounded-lg">
+                      Csevegés
+                    </p>
+                  </Button>
+                  <div
+                    className="relative"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                  ></div>
+                  <Image
+                    width={227}
+                    height={200}
+                    quality={100}
+                    src={tanar.img}
+                    className="min-h-64 max-h-64 min-w-64 max-w-64 rounded-sm hover:opacity-45 hover:bg-gray-500 "
+                  />
+                  <div className="my-3">
+                    <h1>{tanar.name}</h1>
+                    <p className="text-foreground/80">{tanar.caption}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="harmadik">
-            <div className=" border border-black rounded-lg text-center relative  ">
-              <Button
-                onClick={() => (window.location.href = "/turrai/taskovicspeter")}
-                className={`absolute ${
-                  isHovered ? "flex" : "hidden"
-                } items-center justify-center w-full h-64 bg-gray-600 opacity-0 hover:opacity-50`}
-              >
-                <p className="font-bold text-md text-blue-200 border hover:bg-gray-700 bg-black/90 opacity-100 border-gray-200 px-5 py-2 rounded-lg">
-                  Csevegés
-                </p>
-              </Button>
-              <div
-                className="relative"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              ></div>
-              <Image
-                width={227}
-                height={200}
-                quality={100}
-                src="/images/taskovicsAI.png"
-                className="min-h-64 max-h-64 min-w-64 max-w-64 rounded-sm hover:opacity-45 hover:bg-gray-500 "
-              />
-              <div className="my-3">
-                <h1>Taskovics Péter</h1>
-                <p className="text-foreground/80">mérnök informatikus</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="masodik">
-            <div className=" border border-black rounded-lg text-center relative  ">
-              <Button
-                onClick={() => (window.location.href = "/turrai/vargajozsef")}
-                className={`absolute ${
-                  isHovered ? "flex" : "hidden"
-                } items-center justify-center w-full h-64 bg-gray-600 opacity-0 hover:opacity-50`}
-              >
-                <p className="font-bold text-md text-blue-200 border hover:bg-gray-700 bg-black/90 opacity-100 border-gray-200 px-5 py-2 rounded-lg">
-                  Csevegés
-                </p>
-              </Button>
-              <div
-                className="relative"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              ></div>
-              <Image
-                width={227}
-                height={200}
-                quality={100}
-                src="/images/vargaAI.jpg"
-                className="min-h-64 max-h-64 min-w-64 max-w-64 rounded-sm hover:opacity-45 hover:bg-gray-500 "
-              />
-              <div className="my-3">
-                <h1>Varga József</h1>
-                <p className="text-foreground/80">mérnök informatikus</p>
-              </div>
-            </div>
-          </div>
-
-          
+            );
+          })}
         </div>
       </div>
     </>
--- Weboldal/components/Turrmedia/Posztok.jsx
+++ Weboldal/components/Turrmedia/Posztok.jsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Posztok = async () => {
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,profile_picture&access_token=${process.env.INSTAGRAM_KEY}`;
@@ -24,16 +19,20 @@ const Posztok = async () => {
                   <div className="">
                     <div className="flex">
                       <Avatar className="m-1 rounded-sm">
-                        <AvatarImage src="https://scontent.cdninstagram.com/v/t51.2885-19/337339947_1643788049397292_1980261598677310174_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=nkntMBPaAU4AX_JnmJZ&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCAahzcfZzXpH_QI8bJQjfI1cPStPdfoi6tJtfeRpmOeg&oe=65E231EE&_nc_sid=10d13b" alt="@shadcn" />
+                        <AvatarImage
+                          src="https://scontent.cdninstagram.com/v/t51.2885-19/337339947_1643788049397292_1980261598677310174_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=nkntMBPaAU4AX_JnmJZ&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCAahzcfZzXpH_QI8bJQjfI1cPStPdfoi6tJtfeRpmOeg&oe=65E231EE&_nc_sid=10d13b"
+                          alt="@shadcn"
+                        />
                         <AvatarFallback>Türr</AvatarFallback>
                       </Avatar>
-                      <p className=" bg-gray-500/30 h-6 mt-2 rounded-md px-2">Türr100</p>
+                      <p className=" bg-gray-500/30 h-6 mt-2 rounded-md px-2">
+                        Türr100
+                      </p>
                     </div>
                     <div className="flex mx-14 bg-gray-100 rounded-xl -mt-4 w-fit flex-nowrap overflow-hidden  ">
                       <h1 className="">{image.caption}</h1>
                     </div>
                   </div>
-                  
                   <img
                     className="w-1/8"
                     src={image.media_url}
